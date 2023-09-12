51aa0414b4e222b52bcb1ac9c6998da6265ed6c5
O4O110 <114741658+O4O110@users.noreply.github.com>
2023-09-12 17:04:19 +0800

    Update basic.cpp

--- Contents/section1/basic.cpp
+++ Contents/section1/basic.cpp
@@ -1,13 +1,18 @@
 // c++ code
 std::ios::sync_with_stdio(false); // 加速
-
 #include <bits/stdc++.h>
-lower_bound(a, a + n, k);     //最左邊 ≥ k 的位置
-upper_bound(a, a + n, k);     //最左邊 > k 的位置
-upper_bound(a, a + n, k) - 1; //最右邊 ≤ k 的位置
-lower_bound(a, a + n, k) - 1; //最右邊 < k 的位置
-[lower_bound, upper_bound) //等於 k 的範圍
-equal_range(a, a+n, k);
+
+// struct宣告
+struct s{
+  int x[100];
+  int y[100];
+};
+
+s num; //一組s
+num.x[1]=1;
+num.y[1]=2;
+
+// sort
 
 // 從小到大
 priority_queue<int, vector<int>, greater<int>>pq
@@ -16,13 +21,13 @@ insert(it,x)//向vector的任意迭代器it處插入一個元素x
 erase(it)//刪除迭代器爲it處的元素，erase(first,last)
 //刪除一個區間[first,last)內的所有元素，時間複雜度均爲O(N)
 
-set
-insert(x)	//將x插入set中	O(log(n)) 
-count(x)	//回傳x是否存在於set中()	O(log(n)) 
-erase(x)	//刪除在set中的x	O(log(n)) 
-clear()	//刪除set中所有元素	O(n)
-empty()	//回傳是否為空	O(1)
-size()	//回傳共有幾個元素	O(1)
+// set
+s.insert(x)	//將x插入s中	O(log(n)) 
+s.count(x)	//回傳x是否存在於s中()	O(log(n)) 
+s.erase(x)	//刪除在s中的x	O(log(n)) 
+s.clear()	//刪除s中所有元素	O(n)
+s.empty()	//回傳是否為空	O(1)
+s.size()	//回傳共有幾個元素	O(1)
 
 map
 insert(x)	//將x這個pair插入map中	O(log(n)) 
