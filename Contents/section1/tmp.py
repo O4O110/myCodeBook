6f4cd101735f5b8a9f0bce417ce10e926b65cfd5
O4O110 <114741658+O4O110@users.noreply.github.com>
2023-09-12 17:35:51 +0800

    Update tmp.py

--- Contents/section1/tmp.py
+++ Contents/section1/tmp.py
@@ -1,31 +1,60 @@
-sorted((4,1,9,6),reverse=True)
-fruits = ['apple', 'watermelon', 'pear', 'banana']
-a = sorted(fruits, key = lambda x : len(x))
-print(a)
-# 輸出：['pear', 'apple', 'banana', 'watermelon']
-divmod(a,b)
-把除數和餘數運算結果結合起來，
-返回一個包含商和餘數的元組(a // b, a % b)
+# EOF
+while True:
+    try:
+        '''
+        你要執行的程式碼
+        '''
+    except EOFError:
+        break
+	    
+# 有規定終止條件
+while True:
+    if a==0:
+        break
 
-pow(base, exp[, mod])
->>> pow(38, -1, mod=97)
-23
->>> 23 * 38 % 97 == 1
-True
+# 數學符號
+a//=10 # 整除
+a**b   # a^b
 
-eof 寫法
-try:
-	while True:
-		s = input()
-except EOFError:
-	pass
+# 邏輯
+a=True
+b=False
+print(a and b) #False
+print(a or b)  #True
 
-eval(expression, globals=None, locals=None)
+# scan
+a = int(input())
+n=list(input().split(' ')) # 連續輸入一串用空格隔開的數字
 
+for i in range(a):
+    c, d = map(int, input().split()) # 連續輸入兩個數
 
-list(map(int, input().split()))
- L.append(r)
-my_list = ['This' , 'is' , 'a' , 'string' , 'in' , 'Python']
-my_string = " ".join(my_list)
-#This is a string in Python
-test = [[0 for j in range(m)] for i in range(n)]
+# print
+print('for is not a multiple of 11.'.format(a))
+print(a+" and "+b+" sitting in the tree")
+print('The parity of ',a,' is ',count,' (mod 2).')
+
+# 標頭檔math
+import math
+math.gcd(a, b, c, d, e) # 最大公約數
+math.lcm(a, b, c, d, e) # 最小公倍數
+math.fabs(x) # 絕對值
+math.isqrt(n) # 整數平方根
+math.sqrt(x) # 平方根
+math.pow(x, y) # x^y
+
+# count
+c+=b.count("商店")  # 用在要計算好幾個字串時
+c=b.count('1')     # 一次算出一串字串有幾個‘1'
+
+# 進制轉換
+a = bin(a)[2:] # 10 to 2
+a = hex(a)[2:] # 10 to 16
+a = oct(a)[2:] # 10 to 8
+
+# 大小寫轉換
+a.lower()
+a.upper()
+
+# 取長度
+a.len()
